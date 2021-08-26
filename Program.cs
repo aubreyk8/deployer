afe93588d287c1bb0089b58ce605be2ae157197e
Aubrey Hlungwane <Hlungwaneak@invokesolutions.co.za>
2021-08-26 21:52:35 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace deployer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
