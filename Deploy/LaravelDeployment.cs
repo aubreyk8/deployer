acaa43f63dc351e081c560f10feb86e92a89ad0b
Aubrey Hlungwane <Hlungwaneak@invokesolutions.co.za>
2021-08-29 20:23:39 +0200

    Make use of config

--- Deploy/LaravelDeployment.cs
+++ Deploy/LaravelDeployment.cs
@@ -11,7 +11,27 @@ namespace deployer.Deploy
 
         public void Run()
         {
-            throw new System.NotImplementedException();
+            //Update codebase
+            Bash.Run("git pull");
+
+            //Install dependencies based on lock file --no-dev
+            Bash.Run("composer install  --no-interaction --prefer-dist --optimize-autoloader --no-dev");
+
+            if (this.config.Migrate) {
+                //Migrate database
+                Bash.Run("php artisan migrate --force");
+            }
+
+            if (this.config.Optimize) {
+                //# Clear cache
+                Bash.Run("php artisan optimize");
+            }
+
+            if(this.config.Horizon) {
+                // Restart horizon
+                Bash.Run("php artisan horizon:terminate");
+            }
+            
         }
 
         public void Revert(string commitHash)
